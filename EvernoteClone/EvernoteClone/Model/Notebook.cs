d83d548ea80ee2812a84cd4f1f0098d8716ac3da
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-08-21 11:33:10 -0400

    Added the models and database helper.

---
+++ EvernoteClone/EvernoteClone/Model/Notebook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvernoteClone.Model
+{
+    public class Notebook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ EvernoteClone/EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,80 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EvernoteClone.ViewModel.Helpers
+{
+    public class DatabaseHelper
+    {
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rowsAffected = conn.Insert(item);
+
+                if (rowsAffected > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rowsAffected = conn.Update(item);
+
+                if (rowsAffected > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rowsAffected = conn.Delete(item);
+
+                if (rowsAffected > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<T> Read<T>() where T : new()
+        {
+            List<T> items;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                items = conn.Table<T>().ToList();
+            }
+
+            return items;
+        }
+    }
+}
